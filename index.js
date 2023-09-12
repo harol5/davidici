ef6661110d17f5eab3fb4cbc7b6e370d78348b54
harol5 <hrcode95@gmail.com>
2023-09-12 02:03:41 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const tag = document.createElement("script");
+tag.src = "https://www.youtube.com/iframe_api";
+const firstScriptTag = document.getElementsByTagName("script")[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+let player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player("player", {
+    height: "1090",
+    width: "840",
+    videoId: "1qLThLCHf5M",
+    playerVars: {
+      playlist: "1qLThLCHf5M",
+      loop: 1,
+      autoplay: 1,
+      controls: 0,
+    },
+  });
+}
